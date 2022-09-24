7439971bb2e7d62f0fd60cc6e18d74a5ba415288
Lauren <lburke522@gmail.com>
2022-09-24 09:20:51 -0400

    index model

--- models/index.js
+++ models/index.js
@@ -3,13 +3,29 @@ const Post = require('./Post');
 const User = require('./User');
 
 //comment belongsto user
+Comment.belongsto(User, {
+    foreignKey: 'user_id',
+});
 //comment belongsto post
-
+Comment.belongsto(Post, {
+    foreignKey: 'post_id',
+});
 //post belongsto user
+Post.belongsto(User, {
+    foreignKey: 'user_id',
+});
 //post hasmany comment
-
+Post.hasmany(Comment, {
+    foreignKey: 'post_id',
+});
 //user hasmany post
+User.hasmany(Post, {
+    foreignKey: 'user_id',
+});
 //user hasmany comment
+User.hasmany(Comment, {
+    foreignKey: 'user_id',
+});
 
 
 module.exports = { Comment, Post, User };
