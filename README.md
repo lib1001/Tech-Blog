21c333505ef0a90a021967f59c9dbde998d5ef3a
Lauren <lburke522@gmail.com>
2022-09-29 17:38:08 -0400

    final touch

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Tech-Blog
+
+# Tech Blog
+
+## ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Table of Contents:
+◆ [Description](#description)
+
+◆ [Installation](#installation)
+
+◆ [Usage](#usage)
+
+◆ [License](#license)
+
+◆ [Credits](#credits)
+
+◆ [Tests](#tests)
+
+◆ [Contact Information](#contact-information)
+
+## Description:
+
+This application is a tech blog where users can add, update, delete posts.
+
+## Installation:
+
+bcrypt, sequelize, dotenv, express, express-handlebars, express-session, mysql2, path, connect-session-sequelize
+
+## Usage:
+
+[Check out the deployed site!](https://my-tech-blog-lb.herokuapp.com/)
+
+## License:
+
+MIT License
+
+[View License Text](https://opensource.org/licenses/MIT)
+
+
+## Contact Information:
+
+Find me on GitHub at [lib1001](https://github.com/lib1001)!
+
+Email me at [lburke522@gmail.com](mailTo:lburke522@gmail.com).
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,18 +3,7 @@ const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-	Post.findAll({
-	  include: [
-		{
-		  model: Comment,
-		  attributes: ['id', 'comment_content', 'post_id', 'user_id'],
-		},
-		{
-		  model: User,
-		  attributes: ['username'],
-		},
-	  ],
-	})
+	Post.findAll({})
 	  .then((dbPostData) => {
 		const posts = dbPostData.map((post) => post.get({ plain: true }));
 		res.render('homepage', {
@@ -33,16 +22,6 @@ router.get('/', (req, res) => {
 	  where: {
 		id: req.params.id,
 	  },
-	  include: [
-		{
-		  model: Comment,
-		  attributes: ['id', 'comment_content', 'post_id', 'user_id'],
-		},
-		{
-		  model: User,
-		  attributes: ['username'],
-		},
-	  ],
 	})
 	  .then((dbPostData) => {
 		if (!dbPostData) {
@@ -74,44 +53,5 @@ router.get('/', (req, res) => {
   });
   
   
-//   router.get('/posts-comments', (req, res) => {
-// 	Post.findOne({
-// 	  where: {
-// 		id: req.params.id,
-// 	  },
-// 	  attributes: ['id', 'content', 'title', 'created_at'],
-// 	  include: [
-// 		{
-// 		  model: Comment,
-// 		  attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-// 		  include: {
-// 			model: User,
-// 			attributes: ['username'],
-// 		  },
-// 		},
-// 		{
-// 		  model: User,
-// 		  attributes: ['username'],
-// 		},
-// 	  ],
-// 	})
-// 	  .then((dbPostData) => {
-// 		if (!dbPostData) {
-// 		  res.status(404).json({ message: 'No post found with this id' });
-// 		  return;
-// 		}
-// 		const post = dbPostData.get({ plain: true });
-  
-// 		res.render('posts-comments', {
-// 		  post,
-// 		  logged_in: req.session.logged_in,
-// 		  username: req.session.username,
-// 		});
-// 	  })
-// 	  .catch((err) => {
-// 		console.log(err);
-// 		res.status(500).json(err);
-// 	  });
-//   });
 
 module.exports = router;
